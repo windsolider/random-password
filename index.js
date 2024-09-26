87a690bbf3c359be006afabd72162c1d2856dd9e
taocan6 <taocan6@jd.com>
2024-09-26 16:56:43 +0800

    随机生成密码数及随机生成包含大写字母、小写字母、数字或特殊符号的方法

---
+++ index.js
@@ -0,0 +1,62 @@
+function generatePassword(len = 8,useLowercase = true, useUppercase = true, useNumbers = true, useSymbols = false) {
+    const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    let password = [];
+
+    if(useLowercase) password.push(lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)])
+    if(useUppercase) password.push(uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)])
+    if(useNumbers) password.push(numbers[Math.floor(Math.random() * numbers.length)])
+    if(useSymbols) password.push(symbols[Math.floor(Math.random() * symbols.length)])
+
+    let remainingLength = len - password.length;
+
+    let possibleChars = '';
+    if (useLowercase) possibleChars += lowercaseLetters;
+    if (useUppercase) possibleChars += uppercaseLetters;
+    if (useNumbers) possibleChars += numbers;
+    if (useSymbols) possibleChars += symbols;
+    if (possibleChars.length === 0) {
+        throw new Error('At least one character type must be selected.');
+    }
+    for (let i = 0; i < remainingLength; i++) {
+        let index = Math.floor(Math.random() * possibleChars.length);
+        password.push(possibleChars[index]);
+    }
+
+    password = shuffleArray(password);
+
+    return password.join('');
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function getRandomBooleanWithRatio(ratio = 0.5) {
+    if (isNaN(parseFloat(ratio,10))) {
+        throw new Error('Invalid ratio');
+    }
+    if (parseFloat(ratio,10) > 1 || parseFloat(ratio,10) < 0) {
+        throw new Error('Ratio is between 0 and 1');
+    }
+    return Math.random() < ratio;  // 默认50%的概率返回true
+}
+
+function generatePassword3(len=8) {
+    let useNumbers = getRandomBooleanWithRatio();
+    let useSymbols = !useNumbers;
+    return generatePassword(len,true,true,useNumbers,useSymbols)
+}
+
+
+export {
+    generatePassword,
+    generatePassword3
+};
